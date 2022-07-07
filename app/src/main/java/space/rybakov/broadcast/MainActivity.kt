318d3d4d39dd8b3221f3a5a6877aa0c90464a139
dmitry <dimryb@bk.ru>
2022-07-07 22:23:28 +0700

    Через широковещательный Intent передаю проценты из сервиса в Прогрессбар

--- app/src/main/java/space/rybakov/broadcast/MainActivity.kt
+++ app/src/main/java/space/rybakov/broadcast/MainActivity.kt
@@ -1,20 +1,32 @@
 package space.rybakov.broadcast
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ProgressBar
 
 class MainActivity : AppCompatActivity() {
 
-    private val receiver = MyReceiver()
+    private lateinit var progressBar: ProgressBar
+
+    private val receiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            if (intent?.action == "loaded") {
+                val percent = intent.getIntExtra("percent", 0)
+                progressBar.progress = percent
+            }
+        }
+    }
     private var counter = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        progressBar = findViewById(R.id.progressBar)
         findViewById<Button>(R.id.button).setOnClickListener {
             val intent = Intent(MyReceiver.ACTION_CLICKED).apply {
                 putExtra(MyReceiver.EXTRA_COUNT, counter++)
